5c330bf29d75f11a47e3ffb0be5b25b99e59ad68
Max Prosper <maxprosper22@gmail.com>
2022-02-11 09:45:57 +0100

    modifications made

--- backend.py
+++ backend.py
@@ -69,5 +69,6 @@ if __name__ == '__main__':
 cherrypy.config.update({
     'server.socket_host': '0.0.0.0',
     'server.socket_port': int(os.environ.get('PORT', '5000')),
+    'tools.encode.text_only': False,
 })
 cherrypy.quickstart(Backend(), '/', conf)
