8dc90e93a9b539eec68e2e35c513505345118e26
Max Prosper <maxprosper22@gmail.com>
2022-02-11 17:16:05 +0100

    mod backend//

--- pageLoader.py
+++ pageLoader.py
@@ -18,7 +18,7 @@ loader = FileSystemLoader('templates')
 
 env = Environment(loader=loader)
 
-home = env.get_template('home.html').render(data=query())
+home = env.get_template('home.html').render(data=query(), title="My App", name="Max Prosper")
 
 fileName = 'index.html'
 
