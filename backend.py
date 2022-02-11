e48ac75e47411a8cd657d82d87d1d95f1a2449c7
Max Prosper <maxprosper22@gmail.com>
2022-02-11 17:25:22 +0100

    mod backend

--- backend.py
+++ backend.py
@@ -17,7 +17,7 @@ def getproductid():
 
 class Backend:
 	@cherrypy.expose
-	@cherrypy.tools.json_out()
+	#@cherrypy.tools.json_out()
 	def index(self):
 		with open('site/index.html') as f:
 			homepage = f
